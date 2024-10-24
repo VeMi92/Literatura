3c375f645889346b496cd74677634f2ff7fed0a1
VeMi92 <hernestitodelamar@gmail.com>
2024-10-24 02:40:05 -0300

    moldes casi terminados y contenido 30%

---
+++ script(finales).js
@@ -0,0 +1,35 @@
+//*cartas normales*/
+function ucumar(){
+    const ucu = document.querySelector('#ucumar')
+    ucu.style.display = 'flex'
+}
+function duende(){
+    const due = document.querySelector('#duende')
+    due.style.display = 'flex'
+}
+function familiar(){
+    const fam = document.querySelector('#familiar')
+    fam.style.display = 'flex'
+}
+function almamula(){
+    const alm = document.querySelector('#almamula')
+    alm.style.display = 'flex'
+}
+function luzmala(){
+    const luz = document.querySelector('#luzmala')
+    luz.style.display = 'flex'
+}
+function nahuelito(){
+    const nah = document.querySelector('#nahuelito')
+    nah.style.display = 'flex'
+}
+function pombero(){
+    const pom = document.querySelector('#pombero')
+    pom.style.display = 'flex'
+}
+
+//*especial*/
+function gauchogil(){
+    const gau = document.querySelector('#gauchogil')
+    gau.style.display = 'flex'
+}
